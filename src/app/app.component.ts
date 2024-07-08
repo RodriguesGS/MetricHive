d839a7ab83c1cd00e8b1ec7478decbd8d7dd7654
Gabriel Rodrigues <rodriguesgasoares@gmail.com>
2024-07-07 23:00:53 -0300

    Feat: Configurações iniciais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { BodyComponent } from "./components/body/body.component";
 
+interface SideBarToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, SidebarComponent, BodyComponent]
 })
 export class AppComponent {
   title = 'MetricHive';
+
+  isSideBarCollapsed = false;
+  screenWidth = 0;
+
+  onToggleSideBar(data: SideBarToggle):void {
+    this.screenWidth = data.screenWidth;
+    this.isSideBarCollapsed = data.collapsed;
+  }
 }
---
+++ src/app/components/sidebar/nav-data.ts
@@ -0,0 +1,31 @@
+export const navbarData = [
+    {
+        routeLink: 'home',
+        icon: 'assets/img/icons/homeIMG.svg',
+        label: 'Início'
+    },
+
+    {
+        routeLink: 'products',
+        icon: 'assets/img/icons/productIMG.svg',
+        label: 'Produtos'
+    },
+
+    {
+        routeLink: 'client',
+        icon: 'assets/img/icons/clientIMG.svg',
+        label: 'Clientes'
+    },
+
+    {
+        routeLink: 'nfe',
+        icon: 'assets/img/icons/nfeIMG.svg',
+        label: 'Nf-e'
+    },
+
+    {
+        routeLink: 'settings',
+        icon: 'assets/img/icons/settingsIMG.svg',
+        label: 'Configurações'
+    },
+]
