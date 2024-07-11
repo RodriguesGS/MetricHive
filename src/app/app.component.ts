b4a1f7f161e3ba09d994bc0913d7ef85c0087c9b
Gabriel <rodriguesgasoares@gmail.com>
2024-07-11 15:35:57 -0300

    feat: add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { BodyComponent } from "./components/body/body.component";
-
 interface SideBarToggle {
   screenWidth: number;
   collapsed: boolean;
 }
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet, SidebarComponent, BodyComponent]
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [RouterOutlet, SidebarComponent, BodyComponent]
 })
 export class AppComponent {
   title = 'MetricHive';
