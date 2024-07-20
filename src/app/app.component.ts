52dda9e1b3abe993f99ad40e98f6f1a2974e4707
Gabriel Rodrigues <rodriguesgasoares@gmail.com>
2024-07-20 11:52:34 -0300

    feat: add cards home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { BodyComponent } from "./components/body/body.component";
+
 interface SideBarToggle {
   screenWidth: number;
   collapsed: boolean;
 }
-
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, SidebarComponent, BodyComponent],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',   
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, SidebarComponent, BodyComponent]
 })
 export class AppComponent {
   title = 'MetricHive';
@@ -20,7 +20,7 @@ export class AppComponent {
   isSideBarCollapsed = false;
   screenWidth = 0;
 
-  onToggleSideBar(data: SideBarToggle): void {
+  onToggleSideBar(data: SideBarToggle):void {
     this.screenWidth = data.screenWidth;
     this.isSideBarCollapsed = data.collapsed;
   }
