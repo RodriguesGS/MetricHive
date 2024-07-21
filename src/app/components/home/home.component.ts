c77aaf639555f8a40f74100b895a15e18c39aeb5
Gabriel Rodrigues <rodriguesgasoares@gmail.com>
2024-07-21 15:32:15 -0300

    feat: add todo list

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,68 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 
+interface Task {
+  id: number;
+  name: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ MatIconModule ],
+  imports: [ MatIconModule, CommonModule, FormsModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit {
+  newTask: string = '';
+  tasks: Task[] = [];
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  addTask(): void {
+    if (this.newTask.trim()) {
+      const newTask: Task = {
+        id: Date.now(),
+        name: this.newTask.trim(),
+        completed: false
+      };
+      this.tasks.push(newTask);
+      this.saveTasks();
+      this.newTask = '';
+    }
+  }
+
+  toggleTaskCompletion(task: Task): void {
+    task.completed = !task.completed;
+    this.saveTasks();
+  }
+
+  editTask(task: Task): void {
+    const newTaskName = prompt('Editar Tarefa:', task.name);
+    if (newTaskName !== null && newTaskName.trim() !== '') {
+      task.name = newTaskName.trim();
+      this.saveTasks();
+    }
+  }
+
+  deleteTask(task: Task): void {
+    this.tasks = this.tasks.filter(t => t.id !== task.id);
+    this.saveTasks();
+  }
+
+  loadTasks(): void {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      this.tasks = JSON.parse(savedTasks);
+    }
+  }
+
+  saveTasks(): void {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 }
